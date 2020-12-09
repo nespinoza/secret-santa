8b0963d632baad56f21f164630e4b3224f078ad7
nespinoza <nespinoza@stsci.edu>
2020-12-09 14:42:05 -0500

    modified run.py to remove tabs

--- run.py
+++ run.py
@@ -5,11 +5,11 @@ from mail_utils import assign_partner
 from mail_utils import load_emails
 from mail_utils import send_email
 
-
 if __name__ == "__main__":
-	email = email_meta_loader()
-	emails = load_emails(email['list'])
-	assign_partner(emails)
-	for user in emails:
-		print('[INFO] Sending email to {}'.format(user['email']))
-		send_email(email, user)
+        email = email_meta_loader()
+        emails = load_emails(email['list'])
+        assign_partner(emails)
+
+        for user in emails:
+                print('[INFO] Sending email to {}'.format(user['email']))
+                send_email(email, user)
